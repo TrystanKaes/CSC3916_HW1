ccfcdf530ac79fa577db09c8be156bf4905523a8
Trystan Kaes <baby.hughee@gmail.com>
2020-02-06 15:40:28 -0700

    Server listen is causing errors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ server.on("request", (request, response) => {
         console.error(err);
     });
 });
-server.listen(process.env.PORT () => {
+server.listen(process.env.PORT || 8008 () => {
     console.log("Server listening at \$(process.env.PORT)");
 });
 
